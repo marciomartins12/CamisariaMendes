0426d90530c5b8b0879305b2338270da275e489e
marciomartins12 <mmarciom1martins635@gmail.com>
2026-02-07 03:03:16 -0300

    refazendo  css

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -5,6 +5,9 @@ module.exports = {
       whatsappLink: `https://wa.me/${process.env.WHATSAPP_NUMBER}`,
       instagramLink: `https://instagram.com/${process.env.INSTAGRAM_USER}`,
       emailLink: `mailto:${process.env.CONTACT_EMAIL}`,
+      contactEmail: process.env.CONTACT_EMAIL,
+      instagramUser: `@${process.env.INSTAGRAM_USER}`,
+      displayPhone: '(98) 98778-0960' // Hardcoded format based on env for now, or use a library if available
     });
   },
 };
