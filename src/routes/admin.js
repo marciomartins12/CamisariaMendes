f365e42b66b72d2c2298b3a8713fac9e52d716d5
marciomartins12 <mmarciom1martins635@gmail.com>
2026-05-22 23:02:10 -0300

    atualizacao do arquivo exportado

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1842,7 +1842,7 @@ router.get('/campanhas/:id/exportar-word', requireAdmin, async (req, res) => {
                         new TableRow({
                             children: [
                                 new TableCell({
-                                    width: { size: 50, type: WidthType.PERCENTAGE },
+                                    width: { size: 35, type: WidthType.PERCENTAGE },
                                     children: clientContent,
                                     shading: { fill: "FFFFFF" },
                                     borders: {
@@ -1854,7 +1854,7 @@ router.get('/campanhas/:id/exportar-word', requireAdmin, async (req, res) => {
                                     verticalAlign: AlignmentType.TOP
                                 }),
                                 new TableCell({
-                                    width: { size: 50, type: WidthType.PERCENTAGE },
+                                    width: { size: 65, type: WidthType.PERCENTAGE },
                                     children: itemsContent,
                                     shading: { fill: "FAFBFC" },
                                     borders: {
@@ -1887,7 +1887,7 @@ router.get('/campanhas/:id/exportar-word', requireAdmin, async (req, res) => {
                         new TableRow({
                             children: [
                                 new TableCell({
-                                    width: { size: 33, type: WidthType.PERCENTAGE },
+                                    width: { size: 3333, type: WidthType.DXA },
                                     children: [
                                         new Paragraph({
                                             children: [new TextRun({ text: "📅", size: 22 })],
@@ -1915,7 +1915,7 @@ router.get('/campanhas/:id/exportar-word', requireAdmin, async (req, res) => {
                                 }),
                                 
                                 new TableCell({
-                                    width: { size: 34, type: WidthType.PERCENTAGE },
+                                    width: { size: 3334, type: WidthType.DXA },
                                     children: [
                                         new Paragraph({
                                             children: [new TextRun({ text: "💳", size: 22 })],
@@ -1943,7 +1943,7 @@ router.get('/campanhas/:id/exportar-word', requireAdmin, async (req, res) => {
                                 }),
                                 
                                 new TableCell({
-                                    width: { size: 33, type: WidthType.PERCENTAGE },
+                                    width: { size: 3333, type: WidthType.DXA },
                                     children: [
                                         new Paragraph({
                                             children: [new TextRun({ text: "💰", size: 22 })],
